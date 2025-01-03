e7a255b05de1e9513b2266920f051bdb86048abf
CWJ1222 <qadzcwsx@gmail.com>
2025-01-03 16:15:43 +0900

    completeCommit

--- backend/controller/Ctodo.js
+++ backend/controller/Ctodo.js
@@ -33,12 +33,16 @@ exports.readOne = async (req, res) => {
 /* 새로운 Todo 생성 */
 exports.create = async (req, res) => {
   try {
-    console.log(req.body);
+    // console.log(req.body);
     const newTodo = await Todo.create({
       title: req.body.title,
       done: req.body.done,
     });
-    res.send(newTodo);
+    if (title === null && done) {
+      return res.send({ message: "Internal Server Error" });
+    } else {
+      res.send(newTodo);
+    }
   } catch (err) {
     console.log("err", err);
     res.status(500).send("server error");
@@ -46,7 +50,47 @@ exports.create = async (req, res) => {
 };
 
 /* 기존 Todo 수정 */
-exports.update = async (req, res) => {};
+exports.update = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const todoId = req.params.id;
+    if (todoId) {
+      const updatedTodo = await Todo.update(
+        {
+          title: req.body.title,
+          done: req.body.done,
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      );
+      return res.send(updatedTodo);
+    } else {
+      return res.send({ message: "Todo not found" });
+    }
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
+};
 
 /* 기존 Todo 삭제 */
-exports.delete = async (req, res) => {};
+
+exports.delete = async (req, res) => {
+  try {
+    const todoId = req.params.id;
+    const isDeleted = await Todo.destroy({
+      where: {
+        id: todoId,
+      },
+    });
+    if (Boolean(isDeleted)) {
+      res.send("삭제성공");
+    } else res.sned({ message: "Todo not found" });
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("server error");
+  }
+};
